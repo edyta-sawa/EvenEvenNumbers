bb151dcef515809f9ceb9b1557af45b8dc2ee93d
sawae <$$Kacperus16>
2023-10-12 12:18:59 +0200

    even numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int evenVal = 2;
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter last value:");
+        int userVal = userInput.nextInt();
+        do {
+            System.out.println(evenVal);
+            evenVal += 2;
+        } while (evenVal <= userVal);
     }
 }
